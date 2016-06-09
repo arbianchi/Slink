dc19db3d7e6dd7ed12c5fd3d45490485d5936256
Spoonflower <mhowell@spoonflower.com>
2016-06-09 14:13:17 -0400

    added migrations

--- db/migrate/20160609141011_rec_table.rb
+++ db/migrate/20160609141011_rec_table.rb
@@ -1,11 +1,12 @@
-class CreateLinkTable < ActiveRecord::Migration
+class RecTable < ActiveRecord::Migration
   def change
-    create_table :links do |t|
+    create_table :recommendations do |t|
       t.string :title
       t.string :description
       t.string :URL
       t.integer :created_by
       t.timestamps :created_at
+      t.string :posted_at
     end
   end
 end
