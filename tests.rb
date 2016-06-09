44e75fcb309faafb94c5769b8cab2f5591330834
Spoonflower <mhowell@spoonflower.com>
2016-06-09 16:41:33 -0400

    adapting tests

--- tests.rb
+++ tests.rb
@@ -12,15 +12,15 @@ class AppTests < Minitest::Test
   include Rack::Test::Methods
 
   def app
-    ShopDBApp
+    SlinkApp
   end
 
   def setup
-    ShopDBApp.reset_database
+    SlinkApp.reset_database
   end
 
-  def make_item
-    Item.create! description: "Old Busted", price: 3.50
+  def make_link
+    Links.create! title: "facebook", description: "things", URL: "www.facebook.com"
   end
 
 
@@ -32,72 +32,72 @@ class AppTests < Minitest::Test
     header "Authorization", user.password
     assert_equal 0, Item.count
 
-    r = post "/items", description: "New Hotness", price: 100.00
+    r = post "/link", title: "snapchat", description: "New Hotness", URL: "snapchat.com"
 
     assert_equal 200, r.status
-    assert_equal 1, Item.count
-    assert_equal "New Hotness", Item.first.description
+    assert_equal 1, Link.count
+    assert_equal "New Hotness", Link.first.description
   end
 
-  def test_users_can_buy_items
-    item = make_item
-    user = make_existing_user
-    header "Authorization", user.password
+  # def test_users_can_buy_items
+  #   item = make_item
+  #   user = make_existing_user
+  #   header "Authorization", user.password
+  #
+  #   r = post "/items/#{item.id}/buy", quantity: 5
+  #
+  #   assert_equal 200, r.status
+  #   assert_equal 1, Purchase.count
+  #   assert_equal 5, Purchase.first.quantity
+  #   assert_equal Purchase.first, user.purchases.first
+  # end
 
-    r = post "/items/#{item.id}/buy", quantity: 5
+  # def test_users_cant_buy_non_items
+  #   user = make_existing_user
+  #   header "Authorization", user.password
+  #
+  #   assert_raises ActiveRecord::RecordNotFound do
+  #     post "/items/99999/buy", quantity: 5
+  #   end
+  #
+  #   assert_equal 0, Purchase.count
+  # end
 
-    assert_equal 200, r.status
-    assert_equal 1, Purchase.count
-    assert_equal 5, Purchase.first.quantity
-    assert_equal Purchase.first, user.purchases.first
-  end
+  # def test_users_cant_delete_arbitrary_items
+  #   item = make_item
+  #   user = make_existing_user
+  #   header "Authorization", user.password
+  #
+  #   r = delete "/items/#{item.id}"
+  #
+  #   assert_equal 403, r.status
+  #   assert_equal 1, Item.count
+  # end
 
-  def test_users_cant_buy_non_items
-    user = make_existing_user
-    header "Authorization", user.password
-
-    assert_raises ActiveRecord::RecordNotFound do
-      post "/items/99999/buy", quantity: 5
-    end
-
-    assert_equal 0, Purchase.count
-  end
-
-  def test_users_cant_delete_arbitrary_items
-    item = make_item
-    user = make_existing_user
-    header "Authorization", user.password
-
-    r = delete "/items/#{item.id}"
-
-    assert_equal 403, r.status
-    assert_equal 1, Item.count
-  end
-
-  def test_users_can_delete_their_items
-    item = make_item
-    user = make_existing_user
-    header "Authorization", user.password
-
-    item.listed_by = user
-    item.save!
-    r = delete "/items/#{item.id}"
-
-    assert_equal 200, r.status
-    assert_equal 0, Item.count
-  end
-
-  def test_users_can_see_who_has_ordered_an_item
-    item = make_item
-    3.times do |i|
-      u = User.create! first_name: i, last_name: i, password: "pass#{i}"
-      u.purchases.create! item: item, quantity: 4
-    end
-
-    r = get "/items/#{item.id}/purchases"
-
-    assert_equal 200, r.status
-    body = JSON.parse r.body
-    assert_equal 3, body.count
-  end
+  # def test_users_can_delete_their_items
+  #   item = make_item
+  #   user = make_existing_user
+  #   header "Authorization", user.password
+  #
+  #   item.listed_by = user
+  #   item.save!
+  #   r = delete "/items/#{item.id}"
+  #
+  #   assert_equal 200, r.status
+  #   assert_equal 0, Item.count
+  # end
+  #
+  # def test_users_can_see_who_has_ordered_an_item
+  #   item = make_item
+  #   3.times do |i|
+  #     u = User.create! first_name: i, last_name: i, password: "pass#{i}"
+  #     u.purchases.create! item: item, quantity: 4
+  #   end
+  #
+  #   r = get "/items/#{item.id}/purchases"
+  #
+  #   assert_equal 200, r.status
+  #   body = JSON.parse r.body
+  #   assert_equal 3, body.count
+  # end
 end
