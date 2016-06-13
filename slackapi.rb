bf1571c3a7e4448d28cd4ef88cb22e0cd103f3fc
Spoonflower <mhowell@spoonflower.com>
2016-06-12 23:00:01 -0400

    updating slack token

--- slackapi.rb
+++ slackapi.rb
@@ -2,12 +2,11 @@ require 'json'
 require 'pry'
 require 'httparty'
 
-def post_to_slack user, url, recipient
+def post_to_slack rec
 	token = ENV["SLACK_API_KEY"]
-	s = HTTParty.get("https://slack.com/api/chat.postMessage?token=#{token}&channel=%23plock_recommendations&text=#{url}%20%40#{recipient}&link_names=https%3A%2F%2Fslack.com%2F&unfurl_links=true&username=%40#{user}&as_user=true&pretty=1")
+	s = HTTParty.post("https://slack.com/api/chat.postMessage?token=#{token}&channel=C1FJJ2W1F&text=#{rec}&username=Slink&as_user=false&icon_url=http%3A%2F%2Fwww.toysrus.com%2Fgraphics%2Ftru_prod_images%2FSlinky--pTRU1-3025574dt.gif&pretty=1")
 	return s["ok"]
 end
-
 #for testing
 #post_to_slack "adina","www.test.com","maryhowell"
 #puts "success!"
